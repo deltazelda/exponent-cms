1732460d162594006bce59eefc8958067dbc73b0
Phillip Ball <phillip@phillipball.com>
2011-08-11 17:50:33 -0700

    Somemore yui2in3 patches

--- framework/plugins/block.script.php
+++ framework/plugins/block.script.php
@@ -23,28 +23,25 @@ function smarty_block_script($params,$content,&$smarty, &$repeat) {
 		
 		if (empty($params['unique'])) die("<strong style='color:red'>The 'unique' parameter is required for the {script} pluggin.</strong>"); 
 		
-		//exponent_javascript_toFoot($params['unique'],$params['yuimodules'],$smarty->_tpl_vars[__name],$content,$params['src']);
-        // if ((isset($params['yui2mods']) || isset($params['yuimodules'])) && !strstr($content,"YUI(")) {
-        //             $params['yui3mods'] = 1;
-        //             $yui2mods = $params['yui2mods']?$params['yui2mods']:$params['yuimodules'];
-        //             $toreplace = array('"',"'"," ");
-        //             $stripmodquotes = str_replace($toreplace, "", $yui2mods);               
-        //             $splitmods = explode(",",$stripmodquotes);
-        // 
-        //             $y3wrap = "YUI(EXPONENT.YUI3_CONFIG).use(";            
-        //             $y3wrap .= "'yui2-yahoo-dom-event', ";
-        //             foreach ($splitmods as $key=>$mod) {
-        //                 $y3wrap .= "'yui2-".$mod."', ";
-        //             }
-        //             $y3wrap .= "function(Y) {";
-        //             $y3wrap .= "YAHOO=Y.YUI2;";
-        //             $y3wrap .= $content;
-        //             $y3wrap .= "});";
-        //             
-        //             $content = $y3wrap;
-        //             
-        //          eDebug($content);
-        // }
+        if ((isset($params['yui2mods']) || isset($params['yuimodules'])) && !strstr($content,"YUI(")) {
+            $params['yui3mods'] = 1;
+            $yui2mods = $params['yui2mods']?$params['yui2mods']:$params['yuimodules'];
+            $toreplace = array('"',"'"," ");
+            $stripmodquotes = str_replace($toreplace, "", $yui2mods);               
+            $splitmods = explode(",",$stripmodquotes);
+
+            $y3wrap = "YUI(EXPONENT.YUI3_CONFIG).use(";            
+            $y3wrap .= "'yui2-yahoo-dom-event', ";
+            foreach ($splitmods as $key=>$mod) {
+                $y3wrap .= "'yui2-".$mod."', ";
+            }
+            $y3wrap .= "function(Y) {";
+            $y3wrap .= "YAHOO=Y.YUI2;";
+            $y3wrap .= $content;
+            $y3wrap .= "});";
+            
+            $content = $y3wrap;
+        }
 		
         expJavascript::pushToFoot(array(
             "unique"=>$params['unique'],
